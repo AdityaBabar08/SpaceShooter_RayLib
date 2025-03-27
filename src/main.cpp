9bfba9127e3e9a452475e04668bfaa5f94574745
Aditya Babar <babaraditya02@gmail.com>
2025-03-27 13:00:45 +0530

    Player Movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#define RAYMATH_IMPLEMENTATION
 #include "raylib.h"
+#include "raymath.h"
 #include <iostream>
 
 #pragma region imgui
@@ -8,12 +10,37 @@
 #pragma endregion
 
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 450;
+
+struct Player
+{
+	Vector2 playerPos{};
+	Vector2 playerDir{};
+	int playerWidth;
+	int playerHeight;
+};
+
+struct GameData
+{
+	Player player
+	{ 
+		{
+			WINDOW_WIDTH / 2.0f,
+			WINDOW_HEIGHT / 2.0f
+		},
+		{},
+		100,
+		100
+	};
+};
+
 
 int main(void)
 {
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
 
 #pragma region imgui
 	rlImGuiSetup(true);
@@ -27,12 +54,12 @@ int main(void)
 	//imguiThemes::embraceTheDarkness();
 
 
-	ImGuiIO &io = ImGui::GetIO(); (void)io;
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 	io.FontGlobalScale = 2;
 
-	ImGuiStyle &style = ImGui::GetStyle();
+	ImGuiStyle& style = ImGui::GetStyle();
 	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 	{
 		//style.WindowRounding = 0.0f;
@@ -42,6 +69,8 @@ int main(void)
 
 #pragma endregion
 
+	GameData gameData;
+
 
 
 	while (!WindowShouldClose())
@@ -50,29 +79,43 @@ int main(void)
 		ClearBackground(RAYWHITE);
 
 
-	#pragma region imgui
+#pragma region imgui
 		rlImGuiBegin();
 
 		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
 		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
 		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 		ImGui::PopStyleColor(2);
-	#pragma endregion
+#pragma endregion
+
+		
+
+#pragma region Player Movement
+		if (IsKeyDown(KEY_A))
+		{
+			gameData.player.playerPos.x -= 1;
+		}
+		if (IsKeyDown(KEY_D))
+		{
+			gameData.player.playerPos.x += 1;
+		}
+		if (IsKeyDown(KEY_W))
+		{
+			gameData.player.playerPos.y -= 1;
+		}
+		if (IsKeyDown(KEY_S))
+		{
+			gameData.player.playerPos.y += 1;
+		}
+
+		
+#pragma endregion
 
 
-		ImGui::Begin("Test");
+		//DrawCircle(gameData.player.playerPos.x, gameData.player.playerPos.y, 50.f, RED);
+		DrawRectangle(gameData.player.playerPos.x, gameData.player.playerPos.y, gameData.player.playerWidth, gameData.player.playerHeight, GREEN);
 
-		ImGui::Text("Hello");
-		ImGui::Button("Button");
-		ImGui::Button("Button2");
-
-		ImGui::End();
-
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-
-	#pragma region imgui
+#pragma region imgui
 		rlImGuiEnd();
 
 		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
@@ -80,7 +123,7 @@ int main(void)
 			ImGui::UpdatePlatformWindows();
 			ImGui::RenderPlatformWindowsDefault();
 		}
-	#pragma endregion
+#pragma endregion
 
 		EndDrawing();
 	}
